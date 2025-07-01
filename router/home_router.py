7d3774b1d9d52423a8bc8905a1ee31bf3560ec24
Polymercat <144926907+polymercatzz@users.noreply.github.com>
2025-07-01 22:56:32 +0700

    order_mode

--- router/home_router.py
+++ router/home_router.py
@@ -1,6 +1,5 @@
 from fastapi import APIRouter, Request, UploadFile, File, Form
 from fastapi.templating import Jinja2Templates
-from compare import is_similar
 import random
 import os
 import shutil
@@ -49,23 +48,24 @@ async def speaking_mode(request: Request):
 async def play_speaking(request: Request, id: int):
     return templates.TemplateResponse("game_sound2.html", {"request": request})
 
-@router.post("/game_sound/speaking_mode/compare")
-async def compare(request: Request, label: str = Form(...), file: UploadFile = File(...)):
-    UPLOAD_FOLDER = "uploads"
-    upload_path = os.path.join(UPLOAD_FOLDER, file.filename)
-    with open(upload_path, "wb") as f:
-        shutil.copyfileobj(file.file, f)
+# @router.post("/game_sound/speaking_mode/compare")
+# async def compare(request: Request, label: str = Form(...), file: UploadFile = File(...)):
+#     UPLOAD_FOLDER = "uploads"
+#     upload_path = os.path.join(UPLOAD_FOLDER, file.filename)
+#     with open(upload_path, "wb") as f:
+#         shutil.copyfileobj(file.file, f)
 
-    target_path = f"static/sounds/{label}.wav"
-    if not os.path.exists(target_path):
-        return "Find the target file in not found"
+#     target_path = f"static/sounds/{label}.wav"
+#     if not os.path.exists(target_path):
+#         return "Find the target file in not found"
 
-    result, score = is_similar(target_path, upload_path)
-    score_percent = round(float(score) * 100, 2)
-    return {
-        "score": score_percent,
-        "result": "✅ เสียงคล้ายกัน" if result else "❌ ยังไม่ค่อยเหมือน ลองใหม่อีกครั้ง"
-    }
+#     result, score = is_similar(target_path, upload_path)
+#     score_percent = round(float(score) * 100, 2)
+#     os.remove(upload_path)
+#     return {
+#         "score": score_percent,
+#         "result": "✅ เสียงคล้ายกัน" if result else "❌ ยังไม่ค่อยเหมือน ลองใหม่อีกครั้ง"
+#     }
 
 @router.get("/game_sound/my_voice_mode")
 async def my_voice_mode(request: Request):
@@ -83,7 +83,44 @@ async def todo_mode(request: Request):
 
 @router.get("/game_pic/todo_mode/{id}")
 async def play_todo(request: Request, id: int):
-    return templates.TemplateResponse("choosepic.html", {"request": request})
+    todo_list = [
+    {
+        "todo_id": 1,
+        "stage": 1,
+        "path_img": "/static/img/cat.png",
+        "choice_4": ["แมว", "สุนัข", "นก", "ปลา"],
+        "answer": "แมว"
+    },
+    {
+        "todo_id": 2,
+        "stage": 1,
+        "path_img": "/static/img/banana.png",
+        "choice_4": ["ส้ม", "กล้วย", "แอปเปิ้ล", "มะม่วง"],
+        "answer": "กล้วย"
+    },
+    {
+        "todo_id": 3,
+        "stage": 1,
+        "path_img": "/static/img/television.png",
+        "choice_4": ["โทรศัพท์", "วิทยุ", "ทีวี", "ไมโครเวฟ"],
+        "answer": "ทีวี"
+    },
+    {
+        "todo_id": 4,
+        "stage": 1,
+        "path_img": "/static/img/spoon.png",
+        "choice_4": ["มีด", "ตะเกียบ", "ช้อน", "ส้อม"],
+        "answer": "ช้อน"
+    },
+    {
+        "todo_id": 5,
+        "stage": 1,
+        "path_img": "/static/img/car.png",
+        "choice_4": ["รถจักรยาน", "รถไฟ", "รถยนต์", "เครื่องบิน"],
+        "answer": "รถยนต์"
+    }
+]
+    return templates.TemplateResponse("choosepic.html", {"request": request, "todo_list": todo_list})
 
 @router.get("/game_pic/what_you_see_mode")
 async def what_you_see_mode(request: Request):
@@ -99,4 +136,42 @@ async def order_mode(request: Request):
 
 @router.get("/game_pic/order_mode/{id}")
 async def play_order(request: Request, id: int):
-    return templates.TemplateResponse("event_order.html", {"request": request})
+    order_questions = [
+    {
+        "order_id": 1,
+        "stage": 1,
+        "map_choice_4": ["ตื่นนอน", "แปรงฟัน", "ล้างหน้า", "แต่งตัว"]
+    },
+    {
+        "order_id": 2,
+        "stage": 1,
+        "map_choice_4": ["กินข้าวเช้า", "ไปโรงเรียน", "เรียนหนังสือ", "กลับบ้าน"]
+    },
+    {
+        "order_id": 3,
+        "stage": 2,
+        "map_choice_4": ["อาบน้ำ", "ใส่ชุดนอน", "แปรงฟันก่อนนอน", "เข้านอน"]
+    },
+    {
+        "order_id": 4,
+        "stage": 2,
+        "map_choice_4": ["เดินเข้าห้องน้ำ", "กดชักโครก", "ล้างมือ", "ออกจากห้องน้ำ"]
+    },
+    {
+        "order_id": 5,
+        "stage": 3,
+        "map_choice_4": ["หยิบจาน", "ตักข้าว", "นั่งโต๊ะอาหาร", "กินข้าว"]
+    }
+]
+    all_map_choices = []
+    for q in order_questions:
+        choices = list(q["map_choice_4"])
+        random.shuffle(choices)
+
+        all_map_choices.append(choices)
+    print(all_map_choices)
+    return templates.TemplateResponse("event_order.html", {"request": request, "order_questions": order_questions, "all_map_choices": all_map_choices})
+
+@router.get("/submit/{game}/{mode}")
+async def submit(request: Request):
+    return templates.TemplateResponse("sum.html", {"request": request})
