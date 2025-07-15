f3c492cabc7952e3c746ac6e9617511d1bd14d81
kittipith <66070018@kmitl.ac.th>
2025-07-15 19:36:01 +0700

    real tutorial

--- static/javascript/tutorial-main.js
+++ static/javascript/tutorial-main.js
@@ -1,42 +1,36 @@
-const images = [
-    "/static/img/tutorial-main1.png",
-    "/static/img/tutorial-main2.png",
-    "/static/img/tutorial-main3.png",
-    "/static/img/tutorial-main4.png",
-    "/static/img/tutorial-main5.png",
-    "/static/img/tutorial-main6.png",
-    "/static/img/tutorial-main7.png",
-    "/static/img/tutorial-main8.png",
-    "/static/img/tutorial-main9.png"
-];
 let currentSlide = 0;
-let tutorialModal, imageEl, dotsEl;
+let tutorialModal, dotsEl, imageEls;
+
 function renderDots(){
     dotsEl.innerHTML = "";
-    for (let i = 0; i < images.length; i++){
+    for (let i = 0; i < imageEls.length; i++) {
         const dot = document.createElement("span");
-        dot.className = "w-3 h-3 rounded-full mx-1 cursor-pointer transition-all duration-200 " + (i === currentSlide ? "bg-blue-600 scale-110" : "bg-gray-300");
+        dot.className = "w-3 h-3 rounded-full mx-1 cursor-pointer transition-all duration-200 " + 
+                        (i === currentSlide ? "bg-blue-600 scale-110" : "bg-gray-300");
         dot.addEventListener("click", () => {
-        currentSlide = i;
-        showSlide(currentSlide);
+            currentSlide = i;
+            showSlide(currentSlide);
         });
         dotsEl.appendChild(dot);
     }
 }
 function showSlide(index){
-    if (index >= images.length){
+    if (index >= imageEls.length) {
         localStorage.setItem("tutorialSeen", "true");
         closeTutorial();
         return;
     }
-    imageEl.src = images[index];
+    imageEls.forEach((img, i) => {
+        img.classList.toggle("active", i === index);
+    });
+
     renderDots();
 }
 function nextSlide(){
     currentSlide++;
     showSlide(currentSlide);
 }
-function closeTutorial() {
+function closeTutorial(){
     tutorialModal.style.display = "none";
 }
 function openTutorial(force = false){
@@ -48,11 +42,10 @@ function openTutorial(force = false){
 }
 window.addEventListener("DOMContentLoaded", () => {
     tutorialModal = document.getElementById("tutorialModal");
-    imageEl = document.getElementById("tutorialImage");
     dotsEl = document.getElementById("dots");
-        
+    imageEls = Array.from(document.querySelectorAll(".tutorial-slide"));
     const seen = localStorage.getItem("tutorialSeen");
-    if (!seen){
+    if (!seen) {
         openTutorial(true);
     }
 });
